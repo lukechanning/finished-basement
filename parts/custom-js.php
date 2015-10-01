ebaf6f2e55696d53f7bdd38efb22a24a6c63c88f
Luke Patrick <luke@snodandjeff.com>
2015-10-01 04:31:56 +0000

    Adds some changes to the way the middle slider behaves at different DOM widths

--- parts/custom-js.php
+++ parts/custom-js.php
@@ -11,6 +11,23 @@
             items: 4,
             margin:20,
             nav:true,
+            loop: true,
+            dots: false,
             lazyLoad: true,
+                responsive:{
+                0:{
+                    items:1,
+                    nav:true
+                },
+                600:{
+                    items:3,
+                    nav:false
+                },
+                1000:{
+                    items:4,
+                    nav:true,
+                    loop:false
+                }
+            }
         });	 
 </script>
