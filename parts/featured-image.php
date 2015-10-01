abe47414806e60648525936a3faf96d4b9447b4a
Luke Patrick <luke@snodandjeff.com>
2015-10-01 20:22:41 +0000

    Adds updated section for the header lead on interior pages and posts. Uses default image unless featured image is chosen

--- parts/featured-image.php
+++ parts/featured-image.php
@@ -3,6 +3,10 @@
 	if ( has_post_thumbnail( $post->ID ) ) :
 		$image = wp_get_attachment_image_src( get_post_thumbnail_id( $post->ID ), 'single-post-thumbnail' );
 		$image = $image[0];
-		echo '<header id="featured-hero" role="banner" style="background-image: url(\'' . $image .  '\')" ></header>';
+		echo '<header id="featured-hero" role="banner" style="background-image: url(\'' . $image .  '\')">';
+	else:	
+		echo '<header id="featured-hero" role="banner" style="background-image: url(https://images.unsplash.com/photo-1416331108676-a22ccb276e35?q=80&fm=jpg&s=5a8395d0f502a4f63c1aaf8ae3fe0382)">';
 	endif;
-?>
+	?>
+	 <h1 class="entry-title"><?php the_title(); ?></h1>
+	</header>
