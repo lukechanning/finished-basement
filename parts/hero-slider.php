2bb5ef64bd7d030171c42cb8c6c14aa140fcd548
Luke Patrick <luke@snodandjeff.com>
2015-10-01 04:18:35 +0000

    Adds some updates to hero slider, including links and general styling

--- parts/hero-slider.php
+++ parts/hero-slider.php
@@ -4,6 +4,7 @@
   	          query_posts( array ( 'category_name' => 'featured', 'posts_per_page' => -1, 'order' => 'ASC' ) );
 	              //Make sure there are posts to get
 	              while (have_posts()) : the_post();
+	              $permalink = get_permalink();  //Get the permalink
 	                //If it's got a thumbnail, let's get that custom photo
 	                if (has_post_thumbnail( $post->ID ) ): ?>
                       <?php $image = wp_get_attachment_image_src( get_post_thumbnail_id( $post->ID ), 'single-post-thumbnail' ); ?>
@@ -11,6 +12,8 @@
                         <?php else: ?>
                         <section class="hero item">
                     <?php endif; ?>
+                    <a href="<?php echo esc_url($permalink); ?>">
+                      
 	                     <div class="row">
 	                       <?php //Let's get the title side of the deal ?>
 	                       <div class="large-6 large-push-2 columns">
@@ -22,12 +25,14 @@
                           <div class="large-6 large-pull-2 columns">
                             <div class="circle-bg quote-2">
                                 <?php 
-                                the_content();
+                                  echo apply_filters('the_content', substr(get_the_content(), 0, 50) );
+                                  echo " . . . ";
                                 ?>
                             </div>
                           </div>
-                          
 	                     </div>
+	                     
+	                   </a>
 	                  </section>
 	                <?php
 	              endwhile;
