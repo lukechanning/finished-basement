683d31f8f7c13b8c0723ce0ac2bf02c923b4a032
Luke Patrick <luke@snodandjeff.com>
2015-10-08 22:51:54 +0000

    Adds rudimentary sidebar to the listings page, stages for moving to beta. Huzzah

---
+++ parts/cards.php
@@ -0,0 +1,50 @@
+<?php
+	    query_posts( array ( 'post_type' => 'listing', 'posts_per_page' => -1, 'order' => 'ASC' ) );
+	    	while (have_posts()) : the_post(); //Make sure there are posts to get
+	        
+	        	//Let's get the variables that we need
+	            $squarefeet = property_information_get_meta( 'property_information_squarefeet' );
+	        	$bedrooms = property_information_get_meta( 'property_information_bedrooms' );
+	        	$bathrooms = property_information_get_meta( 'property_information_bathrooms' );
+	        	$price = property_information_get_meta( 'property_information_price' );
+	    	
+	    	    //Author specific variables
+	    		$permalink = get_permalink();  //Get the permalink
+	    		$author = get_the_author(); //Snag the author permalink
+	    		$authID = get_the_author_id();
+	    		
+	            //Start the content via HTML
+	            ?>
+	            <div class="item">
+	                <article class="article-card">
+	                    
+	                    <?php the_post_thumbnail('medium'); ?>
+	                    
+	                    <div class="card-content">
+	                        
+	                        <div class="post-icon">
+	                        <?php
+		                        if ( userphoto_exists($authID) ) :
+		                            userphoto_the_author_thumbnail();
+	                            else:
+	                                echo get_avatar($authID, "50px");
+	                            endif;
+	                        ?>
+	                        </div>
+	                       
+	                       <?php get_template_part('parts/info-icons'); ?>
+	                       
+	                        <p class="post-author">Agent <a href="<?php $author ?>"><?php the_author(); ?></a></p>
+	                        <h4><?php the_title(); ?></h4>
+	                        <p><?php echo apply_filters('the_content', substr(get_the_content(), 0, 125) ); ?></p>
+	                        <p>
+	                          <a href="<?php echo esc_url($permalink); ?>">Read on...</a> 
+	                        </p>
+	                    </div>
+	                    
+	                </article>
+	            </div>
+	            <?php
+	            //End the content via HTML
+	        endwhile;		
+	    ?>
--- single-listing.php
+++ single-listing.php
@@ -73,7 +73,7 @@ get_header(); ?>
     				</div>
     				<div class="row">
     					<h4 class="entry-header">Description</h4>
-						<?php the_content(); ?>    			        
+							<?php the_content(); ?>    			        
 					</div>
     			</div>
 			</div>
@@ -83,13 +83,71 @@ get_header(); ?>
 				<p><?php the_tags(); ?></p>
 			</footer>
 		</article>
-	
+	<?php
+	endwhile;
+	wp_reset_query();
+	?>
 
 	<?php do_action( 'foundationpress_after_content' ); ?>
 
 	</div>
-	<?php get_sidebar(); ?>
+	
+		<div class="large-4 small-12 columns sidebar">
+			<h3 class="sidebar-title">Recent Listings</h3>
+			<?php
+			    query_posts( array ( 'post_type' => 'listing', 'posts_per_page' => 2, 'order' => 'ASC' ) );
+			    	while (have_posts()) : the_post(); //Make sure there are posts to get
+			        
+			        	//Let's get the variables that we need
+			            $squarefeet = property_information_get_meta( 'property_information_squarefeet' );
+			        	$bedrooms = property_information_get_meta( 'property_information_bedrooms' );
+			        	$bathrooms = property_information_get_meta( 'property_information_bathrooms' );
+			        	$price = property_information_get_meta( 'property_information_price' );
+			    	
+			    	    //Author specific variables
+			    		$permalink = get_permalink();  //Get the permalink
+			    		$author = get_the_author(); //Snag the author permalink
+			    		$authID = get_the_author_id();
+			    		
+			            //Start the content via HTML
+			            ?>
+			            <div class="item">
+			                <article class="article-card">
+			                    
+			                    <?php the_post_thumbnail('medium'); ?>
+			                    
+			                    <div class="card-content">
+			                        
+			                        <div class="post-icon">
+			                        <?php
+				                        if ( userphoto_exists($authID) ) :
+				                            userphoto_the_author_thumbnail();
+			                            else:
+			                                echo get_avatar($authID, "50px");
+			                            endif;
+			                        ?>
+			                        </div>
+			                       
+			                       <?php get_template_part('parts/info-icons'); ?>
+			                       
+			                        <p class="post-author">Agent <a href="<?php $author ?>"><?php the_author(); ?></a></p>
+			                        <h4><?php the_title(); ?></h4>
+			                        <p><?php echo apply_filters('the_content', substr(get_the_content(), 0, 125) ); ?></p>
+			                        <p>
+			                          <a href="<?php echo esc_url($permalink); ?>">Read on...</a> 
+			                        </p>
+			                    </div>
+			                    
+			                </article>
+			            </div>
+			            <?php
+			            //End the content via HTML
+			        endwhile;		
+			    ?>
+		</div>
+
+	<?php //get_sidebar(); ?>
 </div>
 
-<?php endwhile;?>
+
 <?php get_footer(); ?>
